47fd2742f4d3c57a7dd4171f6fc4d4fb2b28a295
Muhammad Salman <sryt240@gmail.com>
2021-09-23 01:06:19 +0500

    main-2

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-const dropdownLink = document.querySelector('.dropdown')
-const dropdown = document.querySelector('.dropdown-links')
-const headerNavLinks = document.querySelectorAll('.header-navbar li')
+AOS.init({
+  offset: 110,
+  duration: 800,
+});
 
-dropdownLink.addEventListener('click',()=>{
-    dropdown.classList.toggle('active')
-})
+// navbar
 
-headerNavLinks.forEach(navLink =>{
-    navLink.addEventListener('click',()=>{
-        removeAllClasses()
-        navLink.classList.add('active')
+const nav = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > nav.offsetHeight + 100) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+});
+
+const dropdownLink = document.querySelector(".dropdown");
+const dropdown = document.querySelector(".dropdown-links");
+const dropdownLinks = document.querySelectorAll(".dropdown-links a");
+const headerNavLinks = document.querySelectorAll(".header-navbar li");
+
+dropdownLink.addEventListener("click", () => {
+  dropdown.classList.toggle("active");
+});
+
+dropdownLinks.forEach(link =>{
+    link.addEventListener('click',()=>{
+        dropdown.classList.remove('active')
     })
 })
 
-function removeAllClasses(){
-    headerNavLinks.forEach(navLink =>{
-        navLink.classList.remove('active')
-    })
+headerNavLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    removeAllClasses();
+    navLink.classList.add("active");
+  });
+});
+
+function removeAllClasses() {
+  headerNavLinks.forEach((navLink) => {
+    navLink.classList.remove("active");
+  });
 }
+
+
